b2d9f075d421c2a3df3b77ef265671a72c82c49a
bokovium <bokyuri95@gmail.com>
2024-10-22 12:48:55 +0300

    Исправление ошибок линтера

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 //Функция для проверки длины строки
-let checkLine = function (usedLine, maxLength) {
+const checkLine = function (usedLine, maxLength) {
   if (usedLine.length <= maxLength) {
     console.log(true);
   } else {
@@ -11,15 +11,14 @@ checkLine('bokyuri95@gmail.com',20);
 checkLine('LineWithMoreThanTwentySymbols',20);
 
 //Проверка на палиндром
-let line = 'Молебен о коне белом';
-let noBreakLine = line.replaceAll(' ', ''); //Объявление строки без пробелов
+const line = 'Молебен о коне белом';
+const noBreakLine = line.replaceAll(' ', ''); //Объявление строки без пробелов
 console.log(noBreakLine);
-let newLine = noBreakLine.toUpperCase(); //Объявление строки только с верхним регистром и без пробелов
+const newLine = noBreakLine.toUpperCase(); //Объявление строки только с верхним регистром и без пробелов
 console.log(newLine);
 let reverseLine = '';
 
-for (i = newLine.length - 1;i >= 0; i--) { //Создание индекса, считывающего порядковые номера букв в словах
-  newLine[i];
+for (let i = newLine.length - 1;i >= 0; i--) { //Создание индекса, считывающего порядковые номера букв в словах
   reverseLine += newLine[i]; //Добавление очередного символа в строку
   console.log(reverseLine);
 }
