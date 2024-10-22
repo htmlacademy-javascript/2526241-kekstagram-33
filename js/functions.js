991bf672b46d6fb429afc04517d15b15fc259c73
bokovium <bokyuri95@gmail.com>
2024-10-22 23:03:59 +0300

    Очередные исправления по линтерам

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,29 @@
 //Функция для проверки длины строки
+// eslint-disable-next-line no-unused-vars
 const checkLine = function (usedLine, maxLength) {
   if (usedLine.length <= maxLength) {
-    console.log(true);
+    return true;
   } else {
-    console.log(false);
+    return false;
   }
 };
 
-checkLine('bokyuri95@gmail.com',20);
-checkLine('LineWithMoreThanTwentySymbols',20);
-
 //Проверка на палиндром
-const line = 'Молебен о коне белом';
-const noBreakLine = line.replaceAll(' ', ''); //Объявление строки без пробелов
-console.log(noBreakLine);
-const newLine = noBreakLine.toUpperCase(); //Объявление строки только с верхним регистром и без пробелов
-console.log(newLine);
-let reverseLine = '';
 
-for (let i = newLine.length - 1;i >= 0; i--) { //Создание индекса, считывающего порядковые номера букв в словах
-  reverseLine += newLine[i]; //Добавление очередного символа в строку
-  console.log(reverseLine);
-}
 
-if (newLine === reverseLine) { //Сравнение символов у строчек
-  console.log('Это палиндром'); //Вывод сообщения
-} else {
-  console.log('Это не палиндром'); //Вывод сообщения
-}
+// eslint-disable-next-line no-unused-vars
+const checkPalyndrome = function (line) {
+  const noBreakLine = line.replaceAll(' ', '');
+  const newLine = noBreakLine.toUpperCase();
+  let reverseLine = '';
+
+  for (let i = newLine.length - 1;i >= 0; i--) { //Создание индекса, считывающего порядковые номера букв в словах
+    reverseLine += newLine[i]; //Добавление очередного символа в строку
+  }
+
+  if (newLine === reverseLine) { //Сравнение символов у строчек
+    return true; //Вывод сообщения
+  } else {
+    return false; //Вывод сообщения
+  }
+};
